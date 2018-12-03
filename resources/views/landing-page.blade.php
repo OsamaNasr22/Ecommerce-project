cd32c904c103c911868682bdc089772fa38578d1
Osama Nasr <os.ns2013@gmail.com>
2018-12-03 13:21:37 +0200

    make dynamic menus in back end voyager

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -20,18 +20,19 @@
         <header class="with-background">
             <div class="top-nav container">
                 <div class="logo">Laravel Ecommerce</div>
-                <ul>
-                    <li><a href="{{route('shop.index')}}">Shop</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="{{route('cart.index')}}">Cart
-                            @if(Cart::instance('default')->count() > 0)
-                                <span class="cart-count"><span>{{Cart::instance('default')->count()}}</span></span>
+                {{menu('main','partials.menus.main')}}
+                {{--<ul>--}}
+                    {{--<li><a href="{{route('shop.index')}}">Shop</a></li>--}}
+                    {{--<li><a href="#">About</a></li>--}}
+                    {{--<li><a href="#">Blog</a></li>--}}
+                    {{--<li><a href="{{route('cart.index')}}">Cart--}}
+                            {{--@if(Cart::instance('default')->count() > 0)--}}
+                                {{--<span class="cart-count"><span>{{Cart::instance('default')->count()}}</span></span>--}}
 
-                            @endif
-                        </a>
-                    </li>
-                </ul>
+                            {{--@endif--}}
+                        {{--</a>--}}
+                    {{--</li>--}}
+                {{--</ul>--}}
             </div> <!-- end top-nav -->
             <div class="hero container">
                 <div class="hero-copy">
